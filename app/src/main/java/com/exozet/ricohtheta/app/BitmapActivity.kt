24915ae653eec5bc34c2bb0a2835507577863e90
Jan Rabe <jan.rabe@exozet.com>
2019-02-15 14:27:34 +0100

    added example for runtime bitmap changing

---
+++ app/src/main/java/com/exozet/ricohtheta/app/BitmapActivity.kt
@@ -0,0 +1,49 @@
+package com.exozet.ricohtheta.app
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.exozet.threehundredsixtyplayer.loadImage
+import com.exozet.threehundredsixtyplayer.parseAssetFile
+import kotlinx.android.synthetic.main.activity_bitmap.*
+
+class BitmapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bitmap)
+
+        val sample1 = "interior_example.jpg"
+        val sample2 = "equirectangular.jpg"
+        var current = sample2
+
+        var image1: Bitmap? = null
+        var image2: Bitmap? = null
+        var current2: Bitmap?
+
+        sample1.parseAssetFile().loadImage(this) {
+            image1 = it
+            threeHundredSixtyView.bitmap = it
+        }
+
+        sample2.parseAssetFile().loadImage(this) {
+            image2 = it
+        }
+
+        next.setOnClickListener {
+
+            if (current == sample1) {
+                current = sample2
+                current2 = image2
+            } else {
+                current = sample1
+                current2 = image1
+            }
+
+            Log.v("ThreeHundredSixty", "current=$current")
+
+            threeHundredSixtyView.bitmap = current2
+        }
+    }
+}
--- app/src/main/java/com/exozet/ricohtheta/app/MainActivity.kt
+++ app/src/main/java/com/exozet/ricohtheta/app/MainActivity.kt
@@ -2,22 +2,17 @@ package com.exozet.ricohtheta.app
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
-import android.content.Context
-import android.net.*
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.exozet.freedomplayer.FreedomPlayerActivity
-import com.exozet.freedomplayer.Parameter
 import com.exozet.ricohtheta.Theta
 import com.exozet.ricohtheta.cameras.ThetaS
 import com.exozet.ricohtheta.cameras.ThetaV
-import com.exozet.sequentialimageplayer.parseAssetFile
-import com.exozet.threehundredsixty.player.ThreeHundredSixtyPlayer
 import com.tbruyelle.rxpermissions2.RxPermissions
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,12 +21,18 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = Theta::class.java.simpleName
 
+    var disposable: CompositeDisposable? = null
+
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+        disposable = CompositeDisposable()
+
+        show360Player()
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             Theta.createWirelessConnection(this)
         }
 
@@ -40,20 +41,22 @@ class MainActivity : AppCompatActivity() {
                     .subscribeOn(Schedulers.newThread())
                     .observeOn(AndroidSchedulers.mainThread())
                     .doOnError { throwable -> Log.e(TAG, "Throwable ${throwable.message}") }
-                    .subscribe{result -> Log.i(TAG,"snapshot taken $result")}
+                    .subscribe { result -> Log.i(TAG, "snapshot taken $result") }
         }
     }
 
-    private fun show360Player(filename: String) {
-        RxPermissions(this)
+    private fun show360Player() {
+        disposable?.add(RxPermissions(this)
                 .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 .subscribe { granted ->
                     if (granted) {
-                        initFreedomPlayer(parseAssetFile(filename))
+                        startActivity(Intent(this, BitmapActivity::class.java))
+                        finish()
                     } else {
                         // Oups permission denied
                     }
                 }
+        )
     }
 
     override fun onResume() {
@@ -75,25 +78,8 @@ class MainActivity : AppCompatActivity() {
         Theta.onStop()
     }
 
-    fun initFreedomPlayer(threeHundredSixtyUri: Uri) {
-
-        FreedomPlayerActivity.startActivity(this, Parameter(
-                startPlayer = FreedomPlayerActivity.THREE_HUNDRED_SIXTY_PLAYER, // SEQUENTIAL_IMAGE_PLAYER, THREE_HUNDRED_SIXTY_PLAYER, default: FreedomPlayerActivity.SEQUENTIAL_IMAGE_PLAYER
-                // threeHundredSixtyUri = parseAssetFile("equirectangular.jpg"), // load local asset file
-                threeHundredSixtyUri = threeHundredSixtyUri,  // load local asset file
-//                threeHundredSixtyUri = Uri.parse("https://storage.googleapis.com/preview-mobile-de/default/0001/01/b66f74bd094963f1b07b297508cfdeae1262cc7f.json"), // load using interior.json
-                projectionMode = ThreeHundredSixtyPlayer.PROJECTION_MODE_SPHERE, // PROJECTION_MODE_SPHERE, PROJECTION_MODE_MULTI_FISH_EYE_HORIZONTAL, PROJECTION_MODE_MULTI_FISH_EYE_VERTICAL
-                interactionMode = ThreeHundredSixtyPlayer.INTERACTIVE_MODE_MOTION_WITH_TOUCH, // INTERACTIVE_MODE_TOUCH, INTERACTIVE_MODE_MOTION, INTERACTIVE_MODE_MOTION_WITH_TOUCH, default: INTERACTIVE_MODE_MOTION_WITH_TOUCH
-                showControls = false, // shows autoPlay and motion buttons, default false
-                sequentialImageUris = (1 until 192).map { parseAssetFile(String.format("stabilized/out%03d.png", it)) }.toTypedArray(), // load from list of local files
-                // sequentialImageUri = Uri.parse("https://storage.googleapis.com/preview-mobile-de/default/0001/01/7eb02f09747a624a50d3d287d2354610251ec2ad.json"), // load using exterior.json
-                autoPlay = true, // default: true
-                fps = 17, // [1:60] default: 30
-                playBackwards = false, // default: false
-                zoomable = true, // default: true
-                translatable = true, // default: true
-                swipeSpeed = 0.8f, // default 1f
-                blurLetterbox = true // default: true
-        ))
+    override fun onDestroy() {
+        disposable?.dispose()
+        super.onDestroy()
     }
 }
